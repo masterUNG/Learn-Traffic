dec6ef912d2a3487371e4fb65c0c8835fefc9088
masterUNG <phrombutr@gmail.com>
2016-04-03 10:33:11 +0700

    ออกแบบ UI ของ ListView แว้วววว

---
+++ app/src/main/java/appewtc/masterung/learntraffic/MyAdapter.java
@@ -0,0 +1,16 @@
+package appewtc.masterung.learntraffic;
+
+import android.content.Context;
+
+/**
+ * Created by masterUNG on 4/3/16 AD.
+ */
+public class MyAdapter {
+
+    //Explicit
+    private Context context;
+    private String[] titleStrings, descriptionStrings;
+    private int[] iconInts;
+
+
+}   // Main Class
