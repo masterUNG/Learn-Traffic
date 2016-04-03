dda29ce63a3b0564cd0fab1d6ef97ebac71905a5
masterUNG <phrombutr@gmail.com>
2016-04-03 14:11:21 +0700

    data

--- app/src/main/java/appewtc/masterung/learntraffic/MyAdapter.java
+++ app/src/main/java/appewtc/masterung/learntraffic/MyAdapter.java
@@ -1,16 +1,63 @@
 package appewtc.masterung.learntraffic;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by masterUNG on 4/3/16 AD.
  */
-public class MyAdapter {
+public class MyAdapter extends BaseAdapter {
 
     //Explicit
     private Context context;
     private String[] titleStrings, descriptionStrings;
     private int[] iconInts;
 
+    public MyAdapter(Context context, String[] titleStrings, String[] descriptionStrings, int[] iconInts) {
+        this.context = context;
+        this.titleStrings = titleStrings;
+        this.descriptionStrings = descriptionStrings;
+        this.iconInts = iconInts;
+    }
 
+    @Override
+    public int getCount() {
+        return iconInts.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.my_listview, viewGroup, false);
+
+        //For TextView
+        TextView titleTextView = (TextView) view1.findViewById(R.id.textView2);
+        titleTextView.setText(titleStrings[i]);
+
+        TextView descripTextView = (TextView) view1.findViewById(R.id.textView3);
+        descripTextView.setText(descriptionStrings[i]);
+
+        //For Image
+        ImageView iconImageView = (ImageView) view1.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[i]);
+
+
+        return view1;
+    }
 }   // Main Class
---
+++ app/src/main/java/appewtc/masterung/learntraffic/ShowDetail.java
@@ -0,0 +1,34 @@
+package appewtc.masterung.learntraffic;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ShowDetail extends AppCompatActivity {
+
+    //Explicit
+    private TextView titleTextView, detailTextView;
+    //ImageView trafficImageView; เหมือนกับ
+    private ImageView trafficImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_detail);
+
+        //Bind Widget
+        titleTextView = (TextView) findViewById(R.id.textView4);
+        detailTextView = (TextView) findViewById(R.id.textView5);
+        trafficImageView = (ImageView) findViewById(R.id.imageView2);
+
+        //Receive & Show
+        String strTitle = getIntent().getStringExtra("Title");
+        titleTextView.setText(strTitle);
+
+        int intImage = getIntent().getIntExtra("Icon", R.drawable.traffic_01);
+        trafficImageView.setImageResource(intImage);
+
+    }   // Main Method
+
+}   // Main Class
