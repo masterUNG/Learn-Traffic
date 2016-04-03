6de6aef4eaeaeb28fdf7e620d7bf40c183d3f007
masterUNG <phrombutr@gmail.com>
2016-04-03 09:49:35 +0700

    Intent to WebView finish

--- app/src/main/java/appewtc/masterung/learntraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/learntraffic/MainActivity.java
@@ -1,8 +1,10 @@
 package appewtc.masterung.learntraffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -32,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
                 MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cow);
                 mediaPlayer.start();
 
+                //Web View
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/AFmWqLIqDZA"));
+                startActivity(intent);
             }   // onClick
         });
 
